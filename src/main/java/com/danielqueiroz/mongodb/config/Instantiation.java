0dc8e13b0075a65cf5600cd7cf7591bc2a69ab36
queirozdaniel <danielqueirozak77@gmail.com>
2018-04-22 22:15:35 -0300

    Instanciando Objetos no DB

---
+++ src/main/java/com/danielqueiroz/mongodb/config/Instantiation.java
@@ -0,0 +1,30 @@
+package com.danielqueiroz.mongodb.config;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import com.danielqueiroz.mongodb.domain.User;
+import com.danielqueiroz.mongodb.repository.UserRepository;
+
+@Configuration
+public class Instantiation implements CommandLineRunner{
+
+	@Autowired
+	private UserRepository userRepository;
+	
+	@Override
+	public void run(String... arg0) throws Exception {
+		
+		userRepository.deleteAll();
+		
+		User maria = new User(null, "Maria Silva", "mari.s@yahoo.com.br");
+		User jose = new User(null, "José Carlos", "carlos_ze@gmail.com");
+		User daniel = new User(null, "Daniel Queiroz", "danielqz@gmail.com");
+		
+		userRepository.save(Arrays.asList(maria,jose,daniel));
+	}
+
+}
