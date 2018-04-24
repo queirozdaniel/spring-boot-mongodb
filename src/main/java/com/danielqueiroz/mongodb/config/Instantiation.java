d927dddb9c7a36e31e150be7aa2ab3b03f78cc20
queirozdaniel <danielqueirozak77@gmail.com>
2018-04-23 22:33:37 -0300

    Referenciando Posts dos Usuarios

--- src/main/java/com/danielqueiroz/mongodb/config/Instantiation.java
+++ src/main/java/com/danielqueiroz/mongodb/config/Instantiation.java
@@ -45,6 +45,9 @@ public class Instantiation implements CommandLineRunner{
 		
 		postRepository.save(Arrays.asList(post1,post2));
 		
+		maria.getPosts().addAll(Arrays.asList(post1,post2));
+		userRepository.save(maria);
+		
 	}
 
 }
